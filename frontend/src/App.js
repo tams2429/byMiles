fa8b30afaf12c517a116032f0b4fc9339777e73c
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-01-19 17:01:42 +0000

    set up of front end pages and environment

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SignInPage from './components/SignInPage'
+import PolicyPage from './components/PolicyPage'
+
+function App() {
+  return (
+    <>
+      <SignInPage />
+      <PolicyPage />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/PolicyPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PolicyPage = () => {
+  return (
+    <h1>This is the Policy Page</h1>
+  )
+}
+
+export default PolicyPage
